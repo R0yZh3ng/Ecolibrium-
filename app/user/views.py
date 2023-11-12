797c5439e314f4f30ef7bd0fdbec089673a1c3d6
R0yZh3ng <118635905+R0yZh3ng@users.noreply.github.com>
2023-11-12 06:26:55 -0700

    2.1
    merged branches and made a better placeholder for the view personal profile, need a way to pass in the current user_id argument

--- app/user/views.py
+++ app/user/views.py
@@ -48,9 +48,10 @@ def register():
         return redirect(url_for('user.login'))
     return render_template('user/register.html', title = 'Register', form= form)
 
-@user_blueprint.route('/view_profile', methods= ['GET', 'POST'])
+@user_blueprint.route('/view_profile', methods = ['GET', 'POST'])
 def view_profile(user_id):
     user_all_forums = Forum.query.options(db.joinedload(Forum.creator)).filter_by(creator_id=user_id).all()
     username = User.query.get_or_404(user_id)
 
     return render_template('user/view_profile.html', username = username, user_all_forums = user_all_forums)
+
