7f1cded87fb0daaea5246d08871a6867207ff175
MichaelVanderputten <michael.vand3rputten@gmail.com>
2023-11-11 21:17:26 -0700

    initialized database.... roy

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ def create_app():
 
     bcrypt.init_app(app)
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
 
     from .base.views import base_blueprint
--- app/forum/models.py
+++ app/forum/models.py
@@ -3,7 +3,7 @@ from app import db
 from app.user.models import User
 
 class Forum(db.Model):
-    __tablename__ = 'Forum'
+    __tablename__ = 'forum'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), nullable=False)
@@ -12,22 +12,22 @@ class Forum(db.Model):
     created_on = db.Column(db.DateTime, default=datetime.utcnow)
 
     comments = db.relationship('Comment', backref='forum', lazy=True)
-    creator = db.relationship('User', backref='forum')
+    creator = db.relationship('User', backref='forums')
 
 
 class Comment(db.Model):
-    __tablename__ = 'Comment'
+    __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), nullable=False)
     description = db.Column(db.String(1024), nullable=False)
     
     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    Forum_id = db.Column(db.Integer, db.ForeignKey('forum.id'), nullable=False)
+    forum_id = db.Column(db.Integer, db.ForeignKey('forum.id'), nullable=False)
 
     creator = db.relationship('User', backref='comment')
 
     def __repr__(self):
-        return f'<Comment {self.title}>'
+        return f'<comment {self.title}>'
 
 
 def init_db():
